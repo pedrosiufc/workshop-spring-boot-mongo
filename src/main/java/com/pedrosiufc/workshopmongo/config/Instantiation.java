2f8e35cf3968776486a8f83835fd59cc189e97bc
pedrosiufc <pedrosiufc@gmail.com>
2024-09-12 11:09:31 -0300

    Criando entity Post com User aninhado e correção de error no SimpleFormatDate na Instantiation, modificação do nome de variáries e comentários no aplication.properties na busca pela correção

--- src/main/java/com/pedrosiufc/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pedrosiufc/workshopmongo/config/Instantiation.java
@@ -1,31 +1,46 @@
 package com.pedrosiufc.workshopmongo.config;
 
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.pedrosiufc.workshopmongo.domain.Post;
 import com.pedrosiufc.workshopmongo.domain.User;
+import com.pedrosiufc.workshopmongo.repository.PostRepository;
 import com.pedrosiufc.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository userReposiroty;
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... arg0) throws Exception {
 
-		userReposiroty.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
--- src/main/java/com/pedrosiufc/workshopmongo/resouces/UserResource.java
+++ src/main/java/com/pedrosiufc/workshopmongo/resouces/UserResource.java
@@ -6,11 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pedrosiufc.workshopmongo.domain.User;
@@ -18,48 +14,47 @@ import com.pedrosiufc.workshopmongo.dto.UserDTO;
 import com.pedrosiufc.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 	@Autowired
 	private UserService service;
-	
-	@RequestMapping(method=RequestMethod.GET)
- 	public ResponseEntity<List<UserDTO>> findAll() {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
 
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
- 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-	
-		
-	//Inserção
-	@RequestMapping(method=RequestMethod.POST)
- 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+
+	// Inserção
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
-	//Delete
-		@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	 	public ResponseEntity<Void> delete(@PathVariable String id) {
-			service.delete(id);
-			return ResponseEntity.noContent().build();
-		}
-	
-		//update
-		@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
-			User obj = service.fromDTO(objDto);
-			obj.setId(id);
-			obj = service.update(obj);
-			return ResponseEntity.noContent().build();
-		}
-	
+
+	// Delete
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	// update
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
 }
