e50ac6e6dd9296b2bb6c61d8b7f3f7257ab2384f
pedrosiufc <pedrosiufc@gmail.com>
2024-09-13 08:38:17 -0300

    Consulta simples com @query

--- src/main/java/com/pedrosiufc/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pedrosiufc/workshopmongo/repository/PostRepository.java
@@ -3,14 +3,17 @@ package com.pedrosiufc.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pedrosiufc.workshopmongo.domain.Post;
 
-	@Repository
-	public interface PostRepository extends MongoRepository<Post, String> {
-		
-			List<Post> findByTitleContainingIgnoreCase(String text);
-	}
-	
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
 
+	//pega o primeiro parametro com ?0 e i para ignore case buscando no title
+	@Query("\"{ 'title': { $regex: ?0, $options: 'i' } }\"")
+	List<Post> searchTitle(String text);
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
+}
